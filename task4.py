a275a70f23f6fca07af7537386e678aeecff8aca
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-02-28 13:47:53 +0100

    added the questions for task4 in the file

---
+++ task4.py
@@ -0,0 +1,32 @@
+
+"""
+Check the table flights for missing values and think of ways to resolve them.
+"""
+
+"""
+Look for duplicates in the flight table. Take into account that here a flight
+number can occur multiple times, only count it as duplicate when the same flight
+appears multiple times.
+"""
+
+"""
+Convert the (schedueled and actual) arrival departure and departure moments
+to datetime objects.
+"""
+
+"""
+Write a function that checks whether the data in flights is in order. That
+is, verify that the air time , dep time , sched dep time etc. match for each
+flight. If not, think of ways to resolve it if this is not the case.
+"""
+
+"""
+Create a column that contains the local arrival time, incorporating the time
+difference between arrival and departure airport.
+"""
+
+"""
+In addition, information on the different types of planes and airlines will be
+important. Consider studying what the effect of the wind or precipitation is on
+different plane types.
+"""
