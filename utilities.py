69b851e9435714cb7313dc54c32bb45321ca9975
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-07 12:43:42 +0100

    added utilities to use as general tools

---
+++ utilities.py
@@ -0,0 +1,25 @@
+import math
+import sqlite3
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+from datetime import datetime
+
+
+class Data:
+    def __init__(self):
+        self.dp_path = "./Data/flights_database.db"
+        self.make_connection()
+
+    def make_connection(self):
+        self.conn = sqlite3.connect(self.db_path)
+        self.cursor = self.conn.cursor()
+
+    def get_dataframe(self, query, ):
+        self.cursor.execute(query)
+        data_rows = self.cursor.fetchall()
+        df = pd.DataFrame(data_rows, columns=[col[0] for col in self.cursor.description])
+        return df
