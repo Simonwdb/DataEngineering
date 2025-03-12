27517f8b6e3eb5fe639f31f54072ab750b74b604
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-12 14:35:47 +0100

    removed more redundant code

--- main.py
+++ main.py
@@ -39,21 +39,6 @@ def process_flights_data(flights_df, airports_df):
     
     return flights_df
 
-# Perform some data wrangling on the flights_df
-flights_df = remove_nan_values(flights_df)
-remove_duplicates(flights_df)
-convert_time_columns(flights_df)
-adjust_flight_dates(flights_df)
-calculate_delays(flights_df)
-adjust_negative_delays(flights_df)
-check_delay_equality(flights_df)
-
-flights_df = merge_timezone_info(flights_df, airports_df)
-
-convert_arr_date_to_gmt5(flights_df)
-calculate_block_and_taxi_time(flights_df)
-# After this block the flights_df is ready to use as visualization or stastical dataframe 
-
 
 def main():
     print("Here we call the functions from the points described in the functions folder/file")
