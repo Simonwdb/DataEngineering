670813afbfffd8d65626105642e4acd2bf718fb2
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-12 14:27:51 +0100

    made functions for loading the dataframes

--- main.py
+++ main.py
@@ -19,6 +19,28 @@ airlines_df = data_class.get_dataframe(query_airlines)
 query_weather = '''SELECT * from weather'''
 weather_df = data_class.get_dataframe(query_weather)
 
+def get_dataframe_safe(data_class, query):
+    try:
+        return data_class.get_dataframe(query)
+    except Exception as e:
+        return pd.DataFrame()  # Return an empty DataFrame to prevent crashes
+
+def load_data():
+    """Load all necessary data from the database."""
+    data_class = Data()
+    
+    queries = {
+        "flights": "SELECT * FROM flights",
+        "airports": "SELECT * FROM airports",
+        "planes": "SELECT * FROM planes",
+        "airlines": "SELECT * FROM airlines",
+        "weather": "SELECT * FROM weather"
+    }
+    
+    dataframes = {name: get_dataframe_safe(data_class, query) for name, query in queries.items()}
+    
+    return dataframes
+
 # Perform some data wrangling on the flights_df
 flights_df = remove_nan_values(flights_df)
 remove_duplicates(flights_df)
