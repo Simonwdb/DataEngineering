5405bb78a9d7e7b7c1ad35615a2156db4a4b93a1
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-12 14:30:55 +0100

    added function to process flights_df

--- main.py
+++ main.py
@@ -41,6 +41,23 @@ def load_data():
     
     return dataframes
 
+def process_flights_data(flights_df, airports_df):
+    if flights_df.empty:
+        return flights_df
+    
+    flights_df = remove_nan_values(flights_df)
+    flights_df = remove_duplicates(flights_df)
+    flights_df = convert_time_columns(flights_df)
+    flights_df = adjust_flight_dates(flights_df)
+    flights_df = calculate_delays(flights_df)
+    flights_df = adjust_negative_delays(flights_df)
+    flights_df = check_delay_equality(flights_df)
+    flights_df = merge_timezone_info(flights_df, airports_df)
+    flights_df = convert_arr_date_to_gmt5(flights_df)
+    flights_df = calculate_block_and_taxi_time(flights_df)
+    
+    return flights_df
+
 # Perform some data wrangling on the flights_df
 flights_df = remove_nan_values(flights_df)
 remove_duplicates(flights_df)
