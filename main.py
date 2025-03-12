f93d46fcb437480f2ec401565c466d0a3c6742b5
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-12 14:34:25 +0100

    removed redundant code

--- main.py
+++ main.py
@@ -3,32 +3,13 @@ from tasks4 import *
 
 data_class = Data()
 
-# Get the general dataframes from the database
-query_flights = '''SELECT * from flights'''
-flights_df = data_class.get_dataframe(query_flights)
-
-query_airports = '''SELECT * from airports'''
-airports_df = data_class.get_dataframe(query_airports)
-
-query_planes = '''SELECT * from planes'''
-planes_df = data_class.get_dataframe(query_planes)
-
-query_airlines = '''SELECT * from airlines'''
-airlines_df = data_class.get_dataframe(query_airlines)
-
-query_weather = '''SELECT * from weather'''
-weather_df = data_class.get_dataframe(query_weather)
-
 def get_dataframe_safe(data_class, query):
     try:
         return data_class.get_dataframe(query)
     except Exception as e:
         return pd.DataFrame()  # Return an empty DataFrame to prevent crashes
 
-def load_data():
-    """Load all necessary data from the database."""
-    data_class = Data()
-    
+def load_data():    
     queries = {
         "flights": "SELECT * FROM flights",
         "airports": "SELECT * FROM airports",
