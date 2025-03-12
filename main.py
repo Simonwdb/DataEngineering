825b074bb4b10d34f584b5f6872c9169aea3d057
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-12 10:03:25 +0100

    removed redundant code

--- main.py
+++ main.py
@@ -3,29 +3,6 @@ from tasks4 import *
 
 data_class = Data()
 
-'''
-
-POSSIBLE REDUNDANT CODE:
-
-#loading the data
-def get_connection(db_path="./Data/flights_database.db"):
-    conn = sqlite3.connect(db_path)
-    print("Connection to SQLite DB successful")
-    return conn
-
-# making the conn as a global variable
-conn = get_connection()
-
-# Returning a dataframe from a given query
-def get_dataframe(query):
-    cursor = conn.cursor()
-    cursor.execute(query)
-    data_rows = cursor.fetchall()
-    df = pd.DataFrame(data_rows, columns=[col[0] for col in cursor.description])
-    return df
-
-'''
-
 # Get the general dataframes from the database
 query_flights = '''SELECT * from flights'''
 flights_df = data_class.get_dataframe(query_flights)
